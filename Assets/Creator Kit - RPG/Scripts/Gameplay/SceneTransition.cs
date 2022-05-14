1cc743d27d42844443d8e55fdbff034c5abec0f5
chingun <chingun@tomujin.digital>
2022-05-14 22:47:25 +0800

    Upgraded features

--- Assets/Creator Kit - RPG/Scripts/Gameplay/SceneTransition.cs
+++ Assets/Creator Kit - RPG/Scripts/Gameplay/SceneTransition.cs
@@ -5,25 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    public string ScreenName;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
-            SceneManager.LoadScene("SchoolsMap");
+            SceneManager.LoadScene(ScreenName);
         }
 
     }
-
-
 }
