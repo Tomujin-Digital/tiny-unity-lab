acdc92f65f2bf01aba8982dae3c143515e670991
manaljav <manaljav0425@gmail.com>
2022-05-21 14:57:10 +0800

    SchoolsMap Small Changes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+    
+    void Start()
+    {
+        videoPlayer.SetActive(false);        
+    }
+
 
     public void PlayGame()
-    { 
+    {
+        videoPlayer.SetActive(true);
+        Destroy(videoPlayer, timeToStop);
         SceneManager.LoadScene("SchoolsMap"); 
     }
 }
