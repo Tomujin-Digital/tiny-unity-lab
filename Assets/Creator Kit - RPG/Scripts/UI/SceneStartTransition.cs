91556a276d0c51a0e6191422c55d9f4d097fca90
chingun <chingun@tomujin.digital>
2022-05-22 04:26:12 +0800

    latest push by chingun

--- Assets/Creator Kit - RPG/Scripts/UI/SceneStartTransition.cs
+++ Assets/Creator Kit - RPG/Scripts/UI/SceneStartTransition.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneStartTransition : MonoBehaviour
 {
-    private Vector3 targetPosition = new Vector3(0, -10, 0);
+    public Image image;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = targetPosition;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
+        print(image.color);
     }
 }
