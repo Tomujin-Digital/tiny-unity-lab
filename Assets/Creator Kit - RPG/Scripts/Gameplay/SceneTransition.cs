9dfd7815887c1c48d90312d1f27e7fa23db98ae9
chingun <chingun@tomujin.digital>
2022-05-21 11:05:03 +0800

    ziaza

--- Assets/Creator Kit - RPG/Scripts/Gameplay/SceneTransition.cs
+++ Assets/Creator Kit - RPG/Scripts/Gameplay/SceneTransition.cs
@@ -1,22 +1,24 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-
     public string ScreenName;
     public Vector2 PlayerPosition;
     public VectorValue playerStorage;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) {
+        if (collision.CompareTag("Player"))
+        {
             playerStorage.initialValue = PlayerPosition;
             SceneManager.LoadSceneAsync(ScreenName);
-            
         }
-
     }
-
+    public void OnClick(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
 }
